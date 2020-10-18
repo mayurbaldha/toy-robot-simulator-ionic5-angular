96658d5f1b3b9c80b2af4e721d9baa62ebf194b6
Mayur Baldha <mayurbaldha@Mayurs-MacBook-Pro.local>
2020-10-18 12:29:13 +0530

    adding controls on screen

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ActionType } from '../config/action-type.enum';
+import { Action } from '../config/model/action';
+import { ActionService } from '../config/service/action.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public actionTypes = ActionType;
+  constructor(public actionService: ActionService) { }
+  commandClicked($event: Action): void {
+    this.actionService.commands.push($event);
+  }
 
-  constructor() {}
-
+  executeClick(): void {
+    this.actionService.executecommands();
+  }
 }
